adb7f38a55dfd17ab5b9ecf822f6479fd74d2821
Sakshi Patel <55708959+sakshipatel-515@users.noreply.github.com>
2020-10-22 16:02:39 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+        .myimg {
+            width: 20px;
+            height: 20px;
+
+        }
+
+        body {
+            background-image: url(img/stardust.jpg);
+            background-repeat: repeat;
+
+
+        }
+    </style>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>About Mind Benders</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom CSS -->
+    <link href="css/round-about.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+    <div class="container topnav">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand topnav" href="/resume-maker">Resume Maker</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+               
+                <li>
+                    <a href="about.php">About Us</a>
+                </li>
+
+                <!-- <li>
+                     <a href="#contact">Contact</a>
+                 </li> -->
+
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+    <!-- Navigation -->
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Introduction Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">About Mind Benders
+
+                </h1>
+                <p>
+                    <b>We're persuing Bachelor of Technology from Indira Gandhi Delhi Technical University For Women and are interested in developing various interactive and dynamic websites which help people create job-friendly Resumes. We want to be involved in projects that we can be proud of, join some amazing teams and advance our development career. We have always believed that co-creation with users and recruiters is the key to our success. Sensing an oppurtunity to help others with thier job search, we explored possible solutions and build this 'Resume Builder'. By combining our respective backgrounds in Technology, Marketing and Design, we came up with the idea of a web-based tool that would make it as simple as possible to produce Resumes.  <!--<img src="img/yummy.gif">-->
+                    </b>
+
+                </p>
+            </div>
+        </div>
+
+        <!-- Team Members-->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Team</h2>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="img/tanmay.jpg" width="200" height="200" alt="">
+                <h3>Sakshi Patel
+
+                </h3>
+                <p>
+                   <i> Electronics and Communication Engineering Student</i>
+                </p>
+                <p><a href="https://github.com/sakshipatel-515" target="_blank"> <img src="img/social/github.png" class="myimg"> </a>
+                <p><a href="https://www.linkedin.com/in/sakshi-patel-919644188" target="_blank"> <img src="img/social/in.png" class="myimg"> </a>     
+                </p>
+
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="img/darshil.jpg" width="200" height="200" alt="">
+                <h3>Tanjul Jain
+
+                </h3>
+                <p><i>Computer Science Engineering Student</i></p>
+                </p>
+                <p><a href="https://github.com/Tanjul-Jain" target="_blank"> <img src="img/social/github.png" class="myimg"> </a>
+                   <a href="https://www.linkedin.com/in/tanjul-jain-38205317a" target="_blank"> <img src="img/social/in.png" class="myimg"> </a>   
+                </p>
+
+            </div>
+           
+        <hr>
+
+        <!-- Footer -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
